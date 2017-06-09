e7e72ed2aca95e0e99f4f1ea3e883a18ef96a454
cturner314 <turnercharlie25@gmail.com>
2017-06-09 19:08:46 +1000

    fix merge

--- scripts/Snake.js
+++ scripts/Snake.js
@@ -555,11 +555,7 @@ function GenerateAnswer() { 										// generates the question and answer and d
 } 																// END FUNCTION GENERATE ANSWER 
 
 function RndSnakeColor(){
-<<<<<<< HEAD
 	if(MagicSnakeColor == 0){
-=======
-	if(MagicSnakeColor = 0){
->>>>>>> 59587ff06d1341e82fa9ca0237ebfa0afddc499b
 		if(coins > 30){
 			var r, g, b;
 			r = Math.floor((Math.random()*255)+1);
