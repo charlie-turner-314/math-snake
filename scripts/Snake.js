c16c0b540495d3d61a50ea1f6cfcb9cc05121b6b
mathSnake <turnercharlie25+snake@gmail.com>
2018-02-22 09:32:51 +1000

    Update Snake.js
    hopefully remove negative answers

--- scripts/Snake.js
+++ scripts/Snake.js
@@ -564,8 +564,9 @@ function GenerateAnswer() { 										// generates the question and answer and d
 			answer = n1 + n2;
 			break;
 		case 3 : 
-			question = "   " + n1 + " &minus; " + n2;
-			answer = n1 - n2; 
+			if(n2 > n1) question = "   " + n1 + " &minus; " + n2;
+			if(n1 > n2) question = "   " + n2 + " &minus; " + n1;
+			answer = math.Abs(n1 - n2); 
 			break;
 		case 4 :
 			var chosenOpNum = Math.floor((Math.random() * 3) + 1);
@@ -579,8 +580,9 @@ function GenerateAnswer() { 										// generates the question and answer and d
 					answer = n1 - n2;
 					break;
 				case 3:
-					question = "   " + n1 + " &times " + n2;
-					answer = n1 * n2;
+					if(n2 > n1) question = "   " + n1 + " &minus; " + n2;
+					if(n1 > n2) question = "   " + n2 + " &minus; " + n1;
+					answer = math.Abs(n1 - n2); 
 					break;
 			}
 			break;
